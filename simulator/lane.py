7db01b83e40fd4aeaa90e4b65d6d70e82b49a467
Parbat Baniya <parbatbaniya1227@gmail.com>
2025-12-08 20:10:52 +0545

    Created and implemented custom Queue data structure for lanes

---
+++ simulator/lane.py
@@ -0,0 +1,16 @@
+from queue_ds import Queue
+
+
+class Lane:
+    def __init__(self, lane_id):
+        self.lane_id = lane_id
+        self.queue = Queue()
+
+    def add_vehicle(self, vehicle):
+        self.queue.enqueue(vehicle)
+
+    def remove_vehicle(self):
+        self.queue.dequeue()
+
+    def size(self):
+        return self.queue.size()
