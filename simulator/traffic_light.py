fec509d6ef304e081b878788519e8fa198ba3768
Parbat Baniya <parbatbaniya1227@gmail.com>
2025-12-11 16:52:02 +0545

    Changed traffic light states to match the assignment requirement

--- simulator/traffic_light.py
+++ simulator/traffic_light.py
@@ -1,12 +1,15 @@
 class TrafficLight:
     def __init__(self):
-        self.state = 1  # 1 represents Green, 0 represents Red
+        self.state = 1  # 1 represents RED, 2 represents GREEN
 
     def set_green(self):
-        self.state = 1
+        self.state = 2
 
     def set_red(self):
-        self.state = 0
+        self.state = 1
 
     def is_green(self):
-        return self.state == 1
+        return self.state == 2
+
+    def __str__(self):
+        return "GREEN" if self.state == 2 else "RED"
